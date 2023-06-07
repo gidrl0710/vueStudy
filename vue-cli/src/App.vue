3e65a8d7afd222d92d524e372737e86c558d6d89
yeeun <yeeun9507@gmail.com>
2023-06-07 16:20:46 +0900

    2023_06_07

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ str }}
+  </div>
+</template>
+
+<script>
+export default {
+  // 데이터지정 방법
+  data : function() {
+    return { // 새객체반환
+      str: 'Hello'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false, 
+})
+
+module.exports = {
+
+    lintOnSave: false // 단일 단어 컴포넌트 사용 가능
+
+}
